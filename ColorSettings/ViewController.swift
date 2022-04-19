5e139d5d4714c6a3c1978addde00c84e935660a1
BezborodovPavel <bezborodovp@gmail.com>
2022-04-19 15:55:42 +0500

    Task comlete

--- ColorSettings/ViewController.swift
+++ ColorSettings/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var viewForColor: UIView!
+    
+    @IBOutlet weak var redValueSlider: UILabel!
+    @IBOutlet weak var greenValueSlider: UILabel!
+    @IBOutlet weak var bluValueSlider: UILabel!
+    
+    private var redValue: Float = 0 {
+        didSet{
+            let roundedRedValue = round(redValue * 100) / 100
+            redValueSlider.text = String(roundedRedValue)
+            changeColorView(red: redValue, green: greenValue, blue: blueValue)
+        }
+    }
+    
+    private var greenValue: Float = 0 {
+        didSet{
+            let roundedGreenValue = round(greenValue * 100) / 100
+            greenValueSlider.text = String(roundedGreenValue)
+            changeColorView(red: redValue, green: greenValue, blue: blueValue)
+        }
+    }
+    
+    private var blueValue: Float = 0 {
+        didSet{
+            let roundedBlueValue = round(blueValue * 100) / 100
+            bluValueSlider.text = String(roundedBlueValue)
+            changeColorView(red: redValue, green: greenValue, blue: blueValue)
+        }
+    }
+    
+    @IBAction func redSliderChanged(_ sender: UISlider) {
+        redValue = sender.value
     }
 
-
+    @IBAction func greenSliderChanged(_ sender: UISlider) {
+        greenValue = sender.value
+   }
+    
+    @IBAction func bluSliderChanged(_ sender: UISlider) {
+        blueValue = sender.value
+    }
+    
+    override func viewDidLoad() {
+        viewForColor.layer.cornerRadius = 10
+    }
+    
+    private func changeColorView(red: Float, green: Float, blue: Float) {
+        viewForColor.backgroundColor = UIColor(red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: 1)
+    }
 }
 
